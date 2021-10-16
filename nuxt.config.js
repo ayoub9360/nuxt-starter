cf12015f91dfe2276f06efcca08681947ca16990
Ayoub El Guendouz <ayoub.el_guendouz@edu.devinci.fr>
2021-10-16 12:27:13 +0200

    chore(config): Config nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,56 +1,60 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: 'static', // 'server' or 'static'
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Agency',
-    htmlAttrs: {
-      lang: 'en'
-    },
+    title: 'Site_name',
+    noscript: [{ innerHTML: 'This website requires JavaScript.' }],
+    htmlAttrs: { lang: 'fr' },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      // Display number in hyperlink on mobile to call when you click on it
+      { name: 'format-detection', content: 'telephone=no' },
+      // SEO
+      { hid: 'description', name: 'description', content: 'Site_description' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'robots', property: 'robots', content: 'index, follow' },
+      // Windows pin sharing
+      { hid: 'msapplication-TileColor', name: 'msapplication-TileColor', content: '#ffffff' },
+      { hid: 'msapplication-TileImage', name: 'msapplication-TileImage', content: '/favicon.ico' },
+      { hid: 'theme-color', name: 'theme-color', content: '#ffffff' },
+      // How the website display when is sharing on social media
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Site_name' },
+      { hid: 'og:image', property: 'og:image', content: 'https://f.hellowork.com/blogdumoderateur/2013/09/google-logo.png' },
+      { hid: 'og:title', property: 'og:title', content: 'Site_title' },
+      { hid: 'og:description', property: 'og:description', content: 'Site_description' },
+      { hid: 'og:image:alt', property: 'og:image:alt', content: 'Site_description' },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'manifest', href: '/site.webmanifest' },
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/css/reset.css',
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
+  plugins: [],
+
+  components: [
+    { path: '~/components/reusable' },
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/content
     '@nuxt/content',
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {},
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {}
 }
